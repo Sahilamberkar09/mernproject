6f45b9c1a537991d730275b163a8cad04e2196d9
Sahil Amberkar <sahilamberkar2003@gmail.com>
2025-07-26 10:49:01 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -13,8 +13,11 @@ app.use(cors());
 app.use(express.json());
 app.use(express.static(path.join(__dirname, "./frontend/build")));
 
-app.get("*", function (req, res) {
-  res.sendFile(path.join(__dirname, "./frontend/build/index.html"));
+app.get("/.*/", function (req, res) {
+  console.log(
+    "Serving React Frontend from",
+    path.join(__dirname, "./frontend/build/index.html")
+  );
 });
 
 app.get("/", (req, res) => res.send("API Running"));
