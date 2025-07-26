1513c547ad0981b2fbef10649590e37db314a613
Sahil Amberkar <sahilamberkar2003@gmail.com>
2025-07-26 12:29:19 +0530

    Update axios.js

--- frontend/src/libs/axios.js
+++ frontend/src/libs/axios.js
@@ -1,6 +1,5 @@
-const axios = require("axios");
-
-exports.axiosInstance = axios.create({
+import axios from "axios";
+export const axiosInstance = axios.create({
   baseURL: process.env.BASE_URL || "http://localhost:5000/api",
   withCredentials: true,
 });
